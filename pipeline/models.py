2613dabf0aa08d77e495cbbe4b05e9bc29ff0159
cnxls <oles.melinyshyn2007@gmail.com>
2026-02-09 20:47:51 +0200

    Fix models.py

--- pipeline/models.py
+++ pipeline/models.py
@@ -1,13 +1,17 @@
-from pydantic_settings import BaseSettings
-from pydantic import Field
-import datetime 
+from pydantic import BaseModel, Field
+from datetime import datetime
+import json
 
-class Article(BaseSettings):
-    title: str         
-    link: str  
 
+class Article(BaseModel):
+    title: str
+    link: str
     source: str
-    published: str = ""     
-    
+    published: str = ""
     summary: str = ""
-    collected_at: datetime = Field(default_factory=datetime.utcnow)   
+    collected_at: datetime = Field(default_factory=datetime.now)
+
+    @staticmethod
+    def to_json(items_list, filename):
+        with open(filename, 'w') as file:
+            json.dump(items_list, file, indent = 2)
