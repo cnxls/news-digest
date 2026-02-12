35d8792b8987739b9e4d0fe1e16b8bee690d9284
cnxls <oles.melinyshyn2007@gmail.com>
2026-02-12 09:59:17 +0200

    Implement ApiCollector class with collect method

--- pipeline/collectors/api_collector.py
+++ pipeline/collectors/api_collector.py
@@ -1,7 +1,23 @@
 from base import BaseCollector
+import requests
+import datetime
 class ApiCollector(BaseCollector):
-    def __init__():
-        ...
+    
+    BASE_URL = "https://newsapi.org/v2/everything"
+    
+    def __init__(self, api_key: str, query: str = "artificial intelligence"):
+        self.api_key = api_key
+        self.query = query
 
-    def collect():
-        ...
+    def collect(self, ):
+        collector = ApiCollector()
+        params = {"q": self.query,
+                 "pageSize": self.max_articles,
+                 "sortBy": "publishedAt",
+                 "language": "en",
+                 "apiKey": self.api_key}
+    
+        response = requests.get(self.BASE_URL, params=params)   
+        response.raise_for_status()
+
+
