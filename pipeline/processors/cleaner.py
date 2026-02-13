4e1a04683e6b6e86e6ef2024ec62a229b5407f96
cnxls <oles.melinyshyn2007@gmail.com>
2026-02-13 23:45:44 +0200

    Refactor ArticleCleaner class to implement cleaning methods

--- pipeline/processors/cleaner.py
+++ pipeline/processors/cleaner.py
@@ -1,9 +1,27 @@
-class ArticleCleaner:
-    def clean():
-        ...
+import re                          
+from html import unescape          
+from bs4 import BeautifulSoup      
+from pipeline.models import Article 
 
-    #TODO 
-    """
-    rawHTML->BeautifulSoup tag stripping-> html ents decode ->  regex.
-    """
-    
+
+class ArticleCleaner:
+
+    def clean(self, articles: list[Article]) -> list[Article]:       
+        return [self._clean_article(a) for a in articles]
+
+    def _clean_article(self, article: Article) -> Article:           
+        cleaned_summary = self._clean_text(article.summary)          
+        cleaned_content = self._clean_text(article.content)          
+
+        return article.model_copy(update={                           
+            "summary": cleaned_summary,
+            "content": cleaned_content,
+        })
+
+    def _clean_text(self, raw: str) -> str:                          
+        if not raw:                                                  
+            return ""
+
+        text = BeautifulSoup(raw, "html.parser").get_text()          
+        text = unescape(text)                                                         
+        return text
