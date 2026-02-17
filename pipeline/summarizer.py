6245a7bfeaae44ce0e285a7617b8c411fb72e2ef
cnxls <oles.melinyshyn2007@gmail.com>
2026-02-17 21:26:34 +0100

    Refactor build_client method into Summarizer class

--- pipeline/summarizer.py
+++ pipeline/summarizer.py
@@ -2,8 +2,12 @@ from openai import AsyncOpenAI, OpenAIError, RateLimitError
 from anthropic import AsyncAnthropic, APIError, RateLimitError as AnthropicRateLimitError
 from pipeline.config import settings
 
-def build_client(provider_name: str = "openai"):
-    if provider_name == "openai":
-        return "openai", AsyncOpenAI(api_key=settings.openai_api_key)
-    elif provider_name == "anthropic":
-        return "anthropic", AsyncAnthropic(api_key=settings.anthropic_api_key)
+class Summarizer:
+    def __init__(self,  provider_name: str = "openai"):
+        self.provider, self.client = self.build_client(provider_name)
+    
+    def build_client(self, provider_name: str = "openai"):
+        if provider_name == "openai":
+            return "openai", AsyncOpenAI(api_key=settings.openai_api_key)
+        elif provider_name == "anthropic":
+            return "anthropic", AsyncAnthropic(api_key=settings.anthropic_api_key)
