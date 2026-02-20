721fde81be0a1649b9d85af3d76a27b949619280
cnxls <oles.melinyshyn2007@gmail.com>
2026-02-20 22:24:34 +0100

    Refactor ApiCollector's collect method

--- pipeline/collectors/api_collector.py
+++ pipeline/collectors/api_collector.py
@@ -4,34 +4,35 @@ from pipeline.models import Article
 import datetime
 
 class ApiCollector(BaseCollector):
-    
     BASE_URL = "https://newsapi.org/v2/everything"
-    
+
     def __init__(self, api_key: str, query: str = "artificial intelligence"):
         self.api_key = api_key
         self.query = query
 
     def collect(self, max_articles: int = 10) -> list[Article]:
-        params = {"q": self.query,
-                 "pageSize": self.max_articles,
-                 "sortBy": "publishedAt",
-                 "language": "en",
-                 "apiKey": self.api_key}
-    
-        response = requests.get(self.BASE_URL, params=params)   
+        params = {
+            "q": self.query,
+            "pageSize": max_articles,
+            "sortBy": "publishedAt",
+            "language": "en",
+            "apiKey": self.api_key,
+        }
+
+        response = requests.get(self.BASE_URL, params=params)
         response.raise_for_status()
 
         data = response.json()
         results = []
         for new in data['articles']:
             article = Article(
-                    source='NewsApi',
-                    title=new['title'],
-                    link=new['url'],
-                    collected_at=datetime.datetime.now(),
-                    published=new['publishedAt'],
-                    content= new['content'],
-                    summary=new.get("description", "")) 
-            
+                source='NewsApi',
+                title=new['title'],
+                link=new['url'],
+                collected_at=datetime.datetime.now(),
+                published=new['publishedAt'],
+                content=new['content'],
+                summary=new.get("description", ""),
+            )
             results.append(article)
         return results
