64e0cfdeb5d1a64a3a709c453972adfb02c236cf
cnxls <oles.melinyshyn2007@gmail.com>
2026-02-20 21:32:57 +0100

    Add 'collect' command to argument parser and create sources.yaml file

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def main():
     parser = argparse.ArgumentParser(description="AI News Digest")
     subparsers = parser.add_subparsers(dest="command")
 
+    subparsers.add_parser("collect", help="Collect News from config? sources ")
+
 if __name__ == "__main__":
     main()
