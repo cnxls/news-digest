4d220db5a1851a36fbd429b447b92c43693262e4
cnxls <oles.melinyshyn2007@gmail.com>
2026-03-10 14:31:00 +0100

    Fix rss_feed_parse method to include source name for articles in RssCollector

--- pipeline/collectors/rss_collector.py
+++ pipeline/collectors/rss_collector.py
@@ -9,17 +9,22 @@ class RssCollector(BaseCollector):
     def collect(self, max_articles : int = 10) -> list[Article]:
         articles = []
         for feed in self.feeds:
-            art = RssCollector.rss_feed_parse(feed['url'], max_articles)
+            art = RssCollector.rss_feed_parse(feed['url'], feed['name'], max_articles)
             articles.extend(art)
         return articles 
 
     @staticmethod
-    def rss_feed_parse(url : str, number_of_news : int = 50):
+    def rss_feed_parse(url : str, name: str, number_of_news : int = 50):
         feed = feedparser.parse(url)
         news_items = []
         
         for entry in feed.entries[:number_of_news]:  
-            item = Article(title=entry.title, link=entry.link, source=url, published=getattr(entry, 'published', ''), summary=getattr(entry, 'summary', ''))
+            item = Article(title=entry.title,
+                            link=entry.link,
+                            source=name, 
+                            published=getattr(entry, 'published', ''), 
+                            summary=getattr(entry, 'summary', ''))
+            
             news_items.append(item)
         return news_items
 
