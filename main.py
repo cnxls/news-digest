1f2e8ae39bc322e8aa81ff425ff8eea0ffebe331
cnxls <oles.melinyshyn2007@gmail.com>
2026-03-10 20:45:17 +0100

    Add summarize command to main function for article summarization

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(description="AI News Digest")
     subparsers = parser.add_subparsers(dest="command")
 
     subparsers.add_parser("collect", help="Collect News from config sources?")
+    subparsers.add_parser("summarize", help="Summarize articles collected?")
 
     args = parser.parse_args()
 
@@ -15,6 +17,25 @@ def main():
         print(f'Collected and saved into db {len(articles)} articles')
         for article in articles:
             print(article)
+
+    if args.command == 'summarize':
+        import asyncio as aio
+        from pipeline.summarizer import Summarizer
+        from pipeline.database import Database
+        from pipeline.config import settings
+
+        summarizer = Summarizer()
+
+        with Database(database_url=settings.database_url) as db:
+            db.connect()
+            db.init_tables()
+            articles = db.get_todays_articles()
+            
+            prompt = "\n\n".join(articles)
+            summary = aio.run(summarizer.summarize(f"{a['title']}\n {a['content']}" for a in articles))
+            print(summary)
+
+            db.save_digest(summary['text'])
 
 
 if __name__ == "__main__":
