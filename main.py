2bb2c5c7ae05c7cd0310e546d90d0b751f6a93a8
cnxls <oles.melinyshyn2007@gmail.com>
2026-03-11 19:17:22 +0100

    Add scheduling functionality

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
 
     subparsers.add_parser("collect", help="Collect News from config sources?")
     subparsers.add_parser("summarize", help="Summarize articles collected?")
+    subparsers.add_parser("schedule", help = "Run scheduler")
 
     args = parser.parse_args()
 
@@ -35,6 +36,25 @@ def main():
 
             db.save_digest(summary['text'])
 
+    if args.command == 'schedule':
+        from pipeline.scheduler import scheduled_run
+        from pipeline.collectors import run_collect
+        
+        def pipeline_task():
+            run_collect()
 
+            import asyncio as aio
+            from pipeline.database import Database
+            from pipeline.summarizer import Summarizer
+            from pipeline.config import Settings
+            settings = Settings()
+            db = Database(database_url=settings.database_url)
+            scheduled_summarizer = Summarizer()
+            db.init_tables()
+            articles = db.get_todays_articles()
+            scheduled_summary = aio.run(scheduled_summarizer.summarize("\n\n".join(f"{a['title']}\n {a['content']}" for a in articles)))
+            db.save_digest(scheduled_summary['text'])
+
+        scheduled_run(pipeline_task)
 if __name__ == "__main__":
     main()
